03cca3a288c81e5a709ee00973918d4446ca73c8
okkun_sh <okkun.sh@gmail.com>
2021-01-03 21:37:30 +0900

    Add ln command

---
+++ ln.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "%s: noarguments\n", argv[0]);
+    }
+
+    if (link(argv[1], argv[2]) < 0) {
+        perror(argv[1]);
+        exit(1);
+    }
+
+    exit(0);
+}
